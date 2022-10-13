93014ffbe99e5f6cdccfb21f71a3ceda4d81e8e7
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-10-13 16:53:47 -0700

    Save Aqua proxy data to S3 This allows the Lambda functions to complete faster, using less memory. The saved data is deleted after one day - it's unlikely to ever be reused in the first place.

--- api/api/api_stack.py
+++ api/api/api_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import Stack, aws_lambda as lambda_, aws_apigateway as apigw
+from aws_cdk import Duration, Stack, aws_lambda as lambda_, aws_s3 as s3, aws_apigateway as apigw
 from constructs import Construct
 
 
@@ -6,13 +6,50 @@ class ApiStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        aqua_proxy_lambda = lambda_.Function(self, 'AquaProxyLambda', handler='aqua_proxy.handler',
-                                             runtime=lambda_.Runtime.PYTHON_3_8, code=lambda_.Code.from_asset('lambda'))
+        # Create a bucket to store patch data in so that we don't
+        # have Lambdas running for extended periods of time trying
+        # to return large patch files. Even setting a Lambda timeout
+        # of 20s with 512MB of memory wasn't enough to download the
+        # NGS patch list on-demand.
+        aqua_proxy_bucket = s3.Bucket(
+            self,
+            'AquaProxyBucket',
+            access_control=s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            public_read_access=True,
+            lifecycle_rules=[
+                # To ensure that new objects are received on patch days,
+                # objects are created on every request.
+                s3.LifecycleRule(expiration=Duration.days(1)),
+            ],
+        )
 
-        api = apigw.RestApi(self, 'AquaProxyApiGateway',
-                            rest_api_name='AquaProxyApiGateway')
-        api.add_usage_plan('AquaProxyGlobalRateLimit',
-                           throttle=apigw.ThrottleSettings(rate_limit=100))
+        # Create the proxy API
+        aqua_proxy_lambda = lambda_.Function(
+            self,
+            'AquaProxyLambda',
+            handler='aqua_proxy.handler',
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            code=lambda_.Code.from_asset('lambda'),
+            environment={
+                'bucket': aqua_proxy_bucket.bucket_name,
+                'bucket_address': aqua_proxy_bucket.bucket_domain_name
+            },
+        )
+
+        aqua_proxy_bucket.grant_read_write(aqua_proxy_lambda)
+
+        api = apigw.RestApi(
+            self,
+            'AquaProxyApiGateway',
+            rest_api_name='AquaProxyApiGateway',
+        )
+
+        api.add_usage_plan(
+            'AquaProxyGlobalRateLimit',
+            throttle=apigw.ThrottleSettings(rate_limit=100),
+        )
+
         api_entity = api.root.add_resource(
             'aqua-proxy',
             default_cors_preflight_options=apigw.CorsOptions(
--- api/lambda/aqua_proxy.py
+++ api/lambda/aqua_proxy.py
@@ -1,14 +1,43 @@
 from http import client
 import json
 import logging
+import os
 from typing import Dict
 from urllib.parse import urlparse
+from uuid import uuid4
+import boto3
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+s3 = boto3.client('s3')
+bucket = os.environ['bucket']
+bucket_address = os.environ['bucket_address']
+
+
+def get_object_url(key: str) -> str:
+    """
+    Returns the object URL for the object with the provided key.
+    """
+    return f"https://{bucket_address}/{key}"
+
+
+def upload_artifact(artifact: bytes) -> str:
+    """
+    Uploads the provided artifact to S3. The object key will be
+    returned.
+    """
+    key = str(uuid4())
+    s3.put_object(Bucket=bucket, Key=key, Body=artifact)
+    return key
+
 
 def is_url_allowed(url: str) -> bool:
+    """
+    Returns True if the provided URL is a valid game data URL;
+    otherwise, this returns False.
+    """
     parsed_url = urlparse(url)
     if parsed_url.scheme != 'http':
         return False
@@ -18,10 +47,18 @@ def is_url_allowed(url: str) -> bool:
 
 
 def extract_headers(res: client.HTTPResponse) -> Dict[str, str]:
+    """
+    Extracts the headers from the provided response and returns
+    the result as a regular `dict` object.
+    """
     return dict(res.getheaders())
 
 
 def cors_permissive(headers: Dict[str, str]) -> Dict[str, str]:
+    """
+    Adds permissive CORS headers to the provided object, overwriting
+    any existing ones.
+    """
     return {**headers, **{'Access-Control-Allow-Origin': '*'}}
 
 
@@ -78,12 +115,28 @@ def handler(event, context):
         'User-Agent': 'AQUA_HTTP',
     })
 
+    # Get the file from the response and upload it to S3 for
+    # a fast download time
     res = conn.getresponse()
     logger.info('Response status: %d %s', res.status, res.reason)
-    body = str(res.read(), encoding='utf-8')
+    try:
+        object_key = upload_artifact(res.read())
+    except Exception as e:
+        logger.error(e)
+        return {
+            'statusCode': 500,
+            'headers': cors_permissive({
+                'Content-Type': 'application/json',
+            }),
+            'body': json.dumps({
+                'message': 'An error occurred.',
+            }),
+        }
 
     return {
         'statusCode': res.status,
         'headers': cors_permissive(extract_headers(res)),
-        'body': body,
+        'body': json.dumps({
+            'result': get_object_url(object_key),
+        }),
     }
