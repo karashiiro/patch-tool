f78263cfa90629b7f85c7cfa3716145e81324527
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-10-13 22:24:56 -0700

    List launcher files and show sizes

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,29 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 import {useAppDispatch, useAppSelector} from './hooks';
 import {fetchPatchData} from './patches/patchData';
 
+function parseSize(n: number) {
+    const labels = ["B", "MB", "GB"];
+    let i = 0;
+    for (; i < labels.length; i++) {
+        if (n >= 1024) {
+            n /= 1024;
+        } else {
+            break;
+        }
+    }
+    n = Math.floor(n * 100) / 100;
+    return `${n}${labels[i]}`;
+}
+
 function App() {
     const patchData = useAppSelector(state => state.patchData);
     const dispatch = useAppDispatch();
+    useEffect(() => {
+        if (patchData.status === 'not-retrieved') {
+            dispatch(fetchPatchData());
+        }
+    }, [dispatch, patchData.status]);
+
     return (
-        <div className="App">
+        <div>
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <button onClick={() => dispatch(fetchPatchData())}>Fetch</button>
-                <span>{patchData.repositories.master}</span>
+                <ul>
+                    {patchData.launcherFiles.map(f => (
+                        <li>
+                            {f.path} - {parseSize(f.size)}
+                        </li>
+                    ))}
+                </ul>
             </header>
         </div>
     );
